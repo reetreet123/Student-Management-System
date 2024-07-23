d13971d218ff72256aa296fbf05b51df051b7ab1
reetreet123 <123092335+reetreet123@users.noreply.github.com>
2024-07-24 01:28:03 +0530

    Initial Commit

---
+++ src/main/java/net/javaguides/sms/src/main/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,33 @@
+package net.javaguides.sms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception{
+//		Student student1 = new Student("Aafaque", "Alam", "aafaque@gmail.com");
+//		studentRepository.save(student1);
+//		
+//		Student student2 = new Student("Furqan", "Alam", "furqan@gmail.com");
+//		studentRepository.save(student2);
+//		
+//		Student student3 = new Student("Tony", "Stark", "stark@gmail.com");
+//		studentRepository.save(student3);
+	}
+
+}
